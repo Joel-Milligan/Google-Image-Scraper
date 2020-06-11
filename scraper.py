931fbe3ce1be9bc54b3d035cb91143b9d5cc440d
Joel-Milligan <22701252@student.uwa.edu.au>
2020-06-11 23:21:38 +0800

    Added ability to use multiple search terms

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import urllib
-from sys import argv, exit
+from sys import argv
 
 def main():
     argc = len(argv)
@@ -10,12 +10,16 @@ def main():
     if argc == 1:
         search_term = "cat"
         number_of_images = 10
-    elif argc == 2:
-        search_term = argv[1]
-        number_of_images = 10
     else:
         search_term = argv[1]
-        number_of_images = int(argv[2])
+        for i in range(2, argc - 1):
+            search_term += f" {argv[i]}"
+
+        if argv[-1].isnumeric():
+            number_of_images = int(argv[-1])
+        else:
+            search_term += f" {argv[-1]}"
+            number_of_images = 10
 
     google_image_url = f"https://www.google.com/search?q={search_term}&tbm=isch"
     res = requests.get(google_image_url)
@@ -23,8 +27,9 @@ def main():
 
     making_file = True
     i = 1
+    hyphen_search = search_term.replace(" ", "-")
     while making_file:
-        dir_name = f"./{number_of_images}-images-of-{search_term}-v{i}"
+        dir_name = f"./{number_of_images}-images-of-{hyphen_search}-v{i}"
         try:
             os.mkdir(dir_name)
             making_file = False
